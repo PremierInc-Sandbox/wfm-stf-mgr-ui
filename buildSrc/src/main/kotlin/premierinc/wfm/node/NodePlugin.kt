e88baadc66ee5682d5c474b981fdee22c508a43b
jmedd-premier <47331589+jmedd-premier@users.noreply.github.com>
2022-03-17 17:05:10 -0400

    Jacob - AB#20291 - added helm charts and gradle plugins for build config

---
+++ buildSrc/src/main/kotlin/premierinc/wfm/node/NodePlugin.kt
@@ -0,0 +1,60 @@
+package premierinc.wfm.node
+
+import com.github.gradle.node.npm.task.NpmInstallTask
+import com.github.gradle.node.npm.task.NpmTask
+import org.gradle.api.Action
+import com.github.gradle.node.NodeExtension as GradleNodeExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.*
+
+open class NodePlugin : Plugin<Project> {
+  companion object {
+    const val TASK_GROUP = "node"
+  }
+
+  override fun apply(project: Project) = project.run {
+    apply {
+      plugin("com.github.node-gradle.node")
+    }
+    configure<GradleNodeExtension> {
+      download.set(true)
+      distBaseUrl.set("https://code.premierinc.com/artifacts/repository/nodejs/")
+      version.set("14.18.0")
+      npmVersion.set("7.24.0")
+    }
+
+    tasks.apply {
+      val npmInstall by existing(NpmInstallTask::class) {
+        args.set(mutableListOf(
+          "--registry=https://code.premierinc.com/artifacts/repository/npm/"
+        ))
+        execOverrides.set(Action {
+          standardOutput = System.out
+        })
+      }
+
+      val test by registering(NpmTask::class) {
+        group = TASK_GROUP
+        description = "Run frontend tests via `npm run test-headless`"
+        dependsOn(npmInstall)
+        args.set(mutableListOf(
+          "run", "test-headless"
+        ))
+      }
+
+      val buildUI by registering(NpmTask::class) {
+        outputs
+          .dir(layout.projectDirectory.dir("dist"))
+        group = TASK_GROUP
+        description = "Build the UI distribution via `npm run build-cli`"
+        dependsOn(npmInstall, test)
+        args.set(mutableListOf(
+          "run", "build-cli"
+        ))
+      }
+    }
+
+    return@run
+  }
+}
