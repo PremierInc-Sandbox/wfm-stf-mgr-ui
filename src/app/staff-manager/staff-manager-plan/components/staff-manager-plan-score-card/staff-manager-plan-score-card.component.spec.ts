ddff4da608c78f67ce110445b9ac8e0de5b67777
athaniga-pinc <Ashwin_Thanigachalam@premierinc.com>
2022-04-11 22:52:56 +0530

    Update staff-manager-plan-score-card.component.spec.ts

--- src/app/staff-manager/staff-manager-plan/components/staff-manager-plan-score-card/staff-manager-plan-score-card.component.spec.ts
+++ src/app/staff-manager/staff-manager-plan/components/staff-manager-plan-score-card/staff-manager-plan-score-card.component.spec.ts
@@ -139,7 +139,7 @@ describe('StaffManagerPlanScoreCardComponent', () => {
     component.staffVariance=testStaffVarianceData[0];
     component.planDetails=planDetailsTest[0];
     component.getActualWHpU();
-    expect(component.getActualWHpU()).toEqual(9);
+    expect(component.getActualWHpU()).toEqual(8);
   });
   it('should getActualHour', function () {
     let planDetailsTest =planDetailsData();
@@ -147,7 +147,7 @@ describe('StaffManagerPlanScoreCardComponent', () => {
     component.staffVariance=testStaffVarianceData[0];
     component.planDetails=planDetailsTest[0];
     component.getActualHour();
-    expect(component.getActualHour()).toEqual(9);
+    expect(component.getActualHour()).toEqual(8);
   });
   it('should getPlannedMinusDailyHours', function () {
     component.getPlannedMinusDailyHours();
